98389515e7fd0cc59a92c090690812c043f755ef
Zloy_Zliden <91820292+omelhub@users.noreply.github.com>
2022-12-19 16:25:54 +0700

    сделал нормальную MVP с созданием вьюхи из консоли, добавил мультиселекшн

---
+++ WindowsFormsView/IView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharedEventArgs;
+
+namespace WindowsFormsView
+{
+    public interface IView
+    {
+        event EventHandler<StudentAllArgs> EventStudentView;
+        event EventHandler<StudentAddArgs> EventStudentAddView;
+        event EventHandler<StudentDeleteArgs> EventStudentDeleteView;
+
+        void AddStudent((string, string, string) studentInfo);
+        void DeleteStudent(int index);
+    }
+}
